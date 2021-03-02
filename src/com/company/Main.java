b09aa80080fe703fb05b154601d8c53dc2b38198
Pablo Pereira Moure <ppereiramoure@danielcastelao.com>
2021-03-02 11:00:25 +0100

    Añadida libreria como submodulo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
 
+import MiLibreriaV12.FactoriaDeSalidas;
+import MiLibreriaV12.InterfaceMetodosComunes;
+
 public class Main {
 
     public static void main(String[] args) {
 
-    }
+        InterfaceMetodosComunes miSalida;
+        InterfaceMetodosComunes salidaVentana;
+        InterfaceMetodosComunes salidaImpresora;
+
+        miSalida = FactoriaDeSalidas.getProducto(FactoriaDeSalidas.CONSOLA,"Hola desde la fabrica")
+        miSalida.visualiza();
+
+        salidaVentana = FactoriaDeSalidas.getProducto(FactoriaDeSalidas.VENTANA,"Hola desde la fabrica")
+        salidaVentana.visualiza();
+
+        salidaImpresora = FactoriaDeSalidas.getProducto (FactoriaDeSalidas.IMPRESORA,"Hola desde la fabrica")
+        salidaImpresora.visualiza();
+
+
+     }
 }
